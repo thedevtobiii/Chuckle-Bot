d982c1e3826407221a9b7e955fd5d2533bfb03cd
DevTobi <fariuoyetola11@gmail.com>
2025-01-10 22:22:35 +0100

    finished setting up the joke api

--- script.js
+++ script.js
@@ -80,18 +80,37 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: 'd059f30c8fa6459a8b7aa2434867fd94', 
-    src: 'Hello, world!',
-    hl: 'en-us',
-    v: 'Linda',
-    r: 0,
-    c: 'mp3',
-    f: '44khz_16bit_stereo',
-    ssml: false,
-  });
-}
+// function test() {
+//   VoiceRSS.speech({
+//     key: 'd059f30c8fa6459a8b7aa2434867fd94', 
+//     src: 'Hello, world!',
+//     hl: 'en-us',
+//     v: 'Linda',
+//     r: 0,
+//     c: 'mp3',
+//     f: '44khz_16bit_stereo',
+//     ssml: false,
+//   });
+// }
 
-button.addEventListener('click', test);
+// button.addEventListener('click', test);
    
+//get jokes from joke api
+async function getJokes(){
+  let joke='';
+  const apiUrl = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=religious,political,explicit';
+  try{
+const response = await fetch(apiUrl);
+const data = await response.json();
+if (data.setup){
+  joke= `${data.setup}...${data.delivery}`;
+} else {
+  joke = data.joke
+}
+console.log(joke);   
+  } catch (error){   
+    //catch errors here
+    console.log('whoops', error);
+  }  
+}    
+getJokes();      
